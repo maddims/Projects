4467e7ef5fe88461f912ddaffc332ded4e97e1b3
smaddims <phani.maddimsetti@gmail.com>
2022-07-20 11:02:58 +0200

    initial commit - project template

---
+++ src/main/java/com/hcl/db/nacl/NaclApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.db.nacl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaclApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/db/nacl/NaclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.db.nacl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
